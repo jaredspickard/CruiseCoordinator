6c7c63cbfbf3624ad22ade01dba6f9a76cad37d0
Jared Spickard <jaredspickard@berkeley.edu>
2022-01-06 22:53:41 -0800

    more app restructuring

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,58 +1,63 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom'
 import Button from '@material-ui/core/Button';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+
+const googleClientId = '301139010020-rm1mnr8dlnd3656lt8j5f1gv6o001uv6.apps.googleusercontent.com'
 
 class Login extends React.Component {
 
     constructor(props) {
         super(props);
-        this.login = this.login.bind(this);
-        this.authenticatedUser = this.authenticatedUser.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
+        this.handleLoginFailure = this.handleLoginFailure.bind(this);
     }
 
-    async login() {
-        // acquire ThirdPartyToken and save to localStorage
-        await this.getThirdPartyToken();
-        // acquire BackendToken using the ThirdPartyToken and save to localStorage
-        await this.getBackendToken();
-        // rerender the component so successful logins redirect to the home page
-        this.setState({})
+    async handleLogin(googleData) {
+        console.log('successful login');
+        console.log(googleData);
+
+        // TODO: figure out if we want to automatically create user accounts
+        // or redirect them to a page to create their own account
+
+        // fetch user data for our backend 
+        const resp = await fetch('/api/authenticate/google', {
+            method: 'POST',
+            body: JSON.stringify({
+                token: googleData.tokenId
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        //store the user in the client
+        const data = await resp.json();
+        console.log(data);
+        // TODO: figure out how to store the user
     }
 
-    async getThirdPartyToken() {
-        localStorage.setItem('thirdPartyToken', 'abcdef');
-    }
-
-    async getBackendToken() {
-        const thirdPartyToken = localStorage.getItem('thirdPartyToken');
-        if (!thirdPartyToken) {
-            console.log('Unable to retrieve ThirdPartyToken.');
-        }
-        const backendToken = 'ghijk';
-        localStorage.setItem('backendToken', backendToken);
-    }
-
-    authenticatedUser() {
-        const backendToken = localStorage.getItem('backendToken');
-        console.log(backendToken);
-        console.log(Boolean(backendToken));
-        return Boolean(backendToken);
+    async handleLoginFailure(err) {
+        console.log('failed login');
+        console.log(err);
     }
 
     render() {
 
-        const auth = this.authenticatedUser()
-
-        if (auth) {
+        if (false) {
             return <Navigate to='/' />
         } else {
             return (
                 <div>
                     <h2>Login Page</h2>
-                    <Button
-                        variant='contained'
-                        onClick={this.login}
-                    >Login to "Google"</Button>
+                    <div>
+                        <GoogleLogin
+                            clientId={googleClientId}
+                            buttonText='Login with Google'
+                            onSuccess={this.handleLogin}
+                            onFailure={this.handleLoginFailure}
+                            cookiePolicy={'single_host_origin'}
+                        />
+                    </div>
                 </div>
             )
         }
