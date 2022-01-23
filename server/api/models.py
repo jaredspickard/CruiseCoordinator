3d7e024b73f5e0b753966439f75b1f8d1b24a9b7
Jared Spickard <jaredspickard@berkeley.edu>
2022-01-23 11:36:23 -0800

    remove old User model and remove username field from cruiser

--- server/api/models.py
+++ server/api/models.py
@@ -6,7 +6,6 @@ from api import db, login
 
 class Cruiser(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.Text, index=True, unique=True)
     email = db.Column(db.Text, index=True, unique=True)
     password_hash = db.Column(db.Text)
     first_name = db.Column(db.Text)
@@ -47,33 +46,3 @@ class Trip(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     trip_name = db.Column(db.Text)
     coordinator_id = db.Column(db.Integer, db.ForeignKey('cruiser.id'))
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.Text, unique=True)
-    password = db.Column(db.Text)
-    roles = db.Column(db.Text)
-    is_active = db.Column(db.Boolean, default=True, server_default='true')
-
-    @property
-    def rolenames(self):
-        try:
-            return self.roles.split(',')
-        except Exception:
-            return []
-
-    @classmethod
-    def lookup(cls, username):
-        return cls.query.filter_by(username=username).one_or_none()
-
-    @classmethod
-    def identify(cls, id):
-        return cls.query.get(id)
-
-    @property
-    def identity(self):
-        return self.id
-
-    def is_valid(self):
-        return self.is_active
