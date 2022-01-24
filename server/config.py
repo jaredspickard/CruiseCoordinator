1924a78726ab931ce4eb7d180570b18758d2265f
Jared Spickard <jaredspickard@berkeley.edu>
2022-01-23 16:58:27 -0800

    remove sqlalchemy url

--- server/config.py
+++ server/config.py
@@ -3,5 +3,5 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'skadoooooooosh-and-a-lil-kerpluuuuuuunk'
     GOOGLE_CLIENT_ID = '301139010020-rm1mnr8dlnd3656lt8j5f1gv6o001uv6.apps.googleusercontent.com'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/jaredspickard'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/jaredspickard'
     PRAETORIAN_ROLES_DISABLED = True
