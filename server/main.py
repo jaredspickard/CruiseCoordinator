5c8700a50e7e94b2d568f92630be6e9adafcb8fd
Jared Spickard <jaredspickard@berkeley.edu>
2022-01-23 17:50:53 -0800

    define config in main.py

--- server/main.py
+++ server/main.py
@@ -1,14 +1,20 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_heroku import Heroku
-# from flask_praetorian import Praetorian
 # from flask_cors import CORS
-from config import Config
 
 
 app = Flask(__name__)
 app.debug = True
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'skadoooooooosh-and-a-lil-kerpluuuuuuunk'
+    GOOGLE_CLIENT_ID = '301139010020-rm1mnr8dlnd3656lt8j5f1gv6o001uv6.apps.googleusercontent.com'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/jaredspickard'
+    PRAETORIAN_ROLES_DISABLED = True
+
 app.config.from_object(Config)
 
 heroku = Heroku(app)
