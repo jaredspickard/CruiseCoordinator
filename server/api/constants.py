8042771cf2c27ebf762e86e7875022aee78e8914
Jared Spickard <jaredspickard@berkeley.edu>
2022-02-11 17:35:24 -0800

    new constants file

---
+++ server/api/constants.py
@@ -0,0 +1,4 @@
+# constants for cruiser relationships
+FRIENDS = 'friends'
+PENDING_FIRST_SECOND = 'pending_first_second'  # first has sent a friend request to second
+PENDING_SECOND_FIRST = 'pending_second_first'
