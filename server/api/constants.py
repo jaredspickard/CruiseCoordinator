7f15362d395c3f151fb7667f03819f9534cfb0ee
Jared Spickard <jaredspickard@berkeley.edu>
2022-02-12 12:05:18 -0800

    delete constants file and clear __init__ file

--- server/api/constants.py
+++
@@ -1,4 +0,0 @@
-# constants for cruiser relationships
-FRIENDS = 'friends'
-PENDING_FIRST_SECOND = 'pending_first_second'  # first has sent a friend request to second
-PENDING_SECOND_FIRST = 'pending_second_first'
