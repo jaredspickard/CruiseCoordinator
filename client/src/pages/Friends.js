dc2f60a314837cdfe7a157eef79006536a8e9a8c
Jared Spickard <jaredspickard@berkeley.edu>
2022-02-17 18:04:08 -0800

    small FE changes to start with friends page

--- client/src/pages/Friends.js
+++ client/src/pages/Friends.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import FriendsTabs from '../components/FriendsTabs';
 
 class Friends extends React.Component {
     render() {
         return (
             <div>
                 <h2>Friends Page</h2>
+                <FriendsTabs />
             </div>
         )
     }
